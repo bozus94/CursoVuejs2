0d4e947dc0d790e6bd0a346bb24ddbadce40916c
bozus94 <bj_ortega@hotmail.com>
2020-09-19 14:29:59 +0200

    v-show

--- js/scripts.js
+++ js/scripts.js
@@ -1,29 +1,14 @@
 
-let urlUsers = 'https://randomuser.me/api/?results=10';
-let urltodos = "https://jsonplaceholder.typicode.com/todos";
-
 new Vue({
     el: '#appVue',
     created: function () {
         this.getInfo();
     },
     data: {
-        users: [],
-        todos : []
+        email: ''
     },
     methods: {
-        getInfo: function () {
 
-          /* **** metodo para conectarnos usando vue-resource
-            this.$http.get(urltodos).then(function (response) {
-                this.todos = response.body
-            }); */
-
-        /* **** metodo para conectarnos usando axios */
-            axios.get(urltodos).then(reponse => {
-                this.todos = reponse.data
-            })
-        }
     }
 })
 
