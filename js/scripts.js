6b858205cb500ed27505ff1fed4d07b36ea5c59b
bozus94 <bj_ortega@hotmail.com>
2020-09-19 13:59:55 +0200

    vue-resources ajax

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,25 @@
+
+let urlUsers = 'https://randomuser.me/api/?results=10';
+let urltodos = "https://jsonplaceholder.typicode.com/todos";
+
 new Vue({
     el: '#appVue',
-    data: {
-        info: ''
+    created: function () {
+        this.getInfo();
     },
+    data: {
+        users: [],
+        todos : []
+    },
+    methods: {
+        getInfo: function () {
+            this.$http.get(urltodos).then(function (response) {
+                this.todos = response.body
+            });
+        }
+    }
 })
 
+/* 
+    - Para usar ajax primero debemos importar la libreria vue-resources
+*/
